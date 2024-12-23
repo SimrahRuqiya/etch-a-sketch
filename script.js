7ce481d706850ae8aed55ff02daf174a96661274
SimrahRuqiya <123015484+SimrahRuqiya@users.noreply.github.com>
2024-12-23 19:05:30 +0400

    added typewriter text for heading

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Function to write text before game starts
+const txt = "Etch-A-Sketch";
+const speed = 100;
+let i = 0;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("heading").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+typeWriter();
 // JavaScript to create the grid
 const gridContainer = document.getElementById('grid-container');
 
